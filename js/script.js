f302e90b963839e5d1a67b7b219cf0c0b95a1ea8
Michael Myers <moek@devmoek.com>
2022-12-10 21:58:03 +0400

    database

--- js/script.js
+++ js/script.js
@@ -298,4 +298,8 @@ window.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res)); 
 });
