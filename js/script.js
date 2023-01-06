cc743db350f14065e7ce13e73a0d1cbb89eb656b
Michael Myers <moek@devmoek.com>
2023-01-06 19:11:06 +0400

    small changes

--- js/script.js
+++ js/script.js
@@ -388,10 +388,10 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length -1)) {
+        if (offset == +width.replace(/\D/g, '') * (slides.length -1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2)
+            offset += +width.replace(/\D/g, '');
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -414,9 +414,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = +width.replace(/\D/g, '') * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= +width.replace(/\D/g, '');
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -442,7 +442,7 @@ window.addEventListener("DOMContentLoaded", () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = +width.replace(/\D/g, '') * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
